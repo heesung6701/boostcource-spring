a8a0992b1565018aa96a0c8afc88008e2a2abd9f
윤희성 <heesung6701@naver.com>
2020-06-27 17:23:10 +0900

    Utility classes should not have public constructors (java:S1118)

--- 프로젝트A. 명함 관리 프로그램/cardmanager/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDaoSqls.java
+++ 프로젝트A. 명함 관리 프로그램/cardmanager/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDaoSqls.java
@@ -1,6 +1,10 @@
 package org.edwith.webbe.cardmanager.dao;
 
 public class BusinessCardManagerDaoSqls {
-  public static String INSERT = "INSERT INTO business_card(name, phone, company_name) VALUES ( ?, ?, ? )";
-  public static String SELECT_LIKE = "SELECT * FROM business_card WHERE NAME LIKE ?";
+  private BusinessCardManagerDaoSqls() {
+    throw new IllegalStateException("Utility class");
+  }
+  
+  public static final String INSERT = "INSERT INTO business_card(name, phone, company_name) VALUES ( ?, ?, ? )";
+  public static final String SELECT_LIKE = "SELECT * FROM business_card WHERE NAME LIKE ?";
 }
