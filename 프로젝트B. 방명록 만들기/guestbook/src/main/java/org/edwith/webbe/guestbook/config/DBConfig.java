09d51ee9eff7efc03bc6a89e2f2d4ba03a4589af
윤희성 <heesung6701@naver.com>
2020-06-28 10:01:58 +0900

    ADD DB config file
    only for localhost

---
+++ 프로젝트B. 방명록 만들기/guestbook/src/main/java/org/edwith/webbe/guestbook/config/DBConfig.java
@@ -0,0 +1,7 @@
+package org.edwith.webbe.guestbook.config;
+
+public class DBConfig {
+  public static String dburl = "jdbc:mysql://localhost:3306/project_b?useUnicode=true&characterEncoding=utf8&useSSL=fals&?serverTimezone=UTC";
+  public static String dbUser = "connectuser";
+  public static String dbpasswd = "connect123!@#";
+}
