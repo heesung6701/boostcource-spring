40631e649c23546e16f11db3b0ef14959298c421
윤희성 <heesung6701@naver.com>
2020-06-28 13:02:28 +0900

    fix typo error

--- 프로젝트B. 방명록 만들기/guestbook/src/main/java/org/edwith/webbe/guestbook/config/DBConfig.java
+++ 프로젝트B. 방명록 만들기/guestbook/src/main/java/org/edwith/webbe/guestbook/config/DBConfig.java
@@ -2,7 +2,7 @@ package org.edwith.webbe.guestbook.config;
 
 public class DBConfig {
   public static String dburl =
-      "jdbc:mysql://localhost:3306/project_b?useUnicode=true&characterEncoding=utf8&useSSL=fals&?serverTimezone=UTC";
+      "jdbc:mysql://localhost:3306/project_b?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
   public static String dbUser = "connectuser";
   public static String dbpasswd = "connect123!@#";
 }
