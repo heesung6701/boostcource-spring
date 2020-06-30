035d3f6b1ec457f80f67ca2f4f68600ea9664c34
윤희성 <heesung6701@naver.com>
2020-06-30 21:47:02 +0900

    ADD "select all category "

---
+++ 프로젝트C. 예약  메인, 상세보기 관련 web API 만들기/reservation/src/main/java/kr/or/connect/reservation/dao/CategoryDao.java
@@ -0,0 +1,25 @@
+package kr.or.connect.reservation.dao;
+
+import static kr.or.connect.reservation.dao.CategoryDaoSqls.SELECT_ALL;
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import kr.or.connect.reservation.dto.CategoryDto;
+
+@Repository
+public class CategoryDao {
+
+  private NamedParameterJdbcTemplate jdbc;
+  private RowMapper<CategoryDto> rowMapper = BeanPropertyRowMapper.newInstance(CategoryDto.class);
+
+  public CategoryDao(DataSource dataSource){
+      this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+  }
+
+  public List<CategoryDto> selectAll() {
+    return jdbc.query(SELECT_ALL, rowMapper);  
+  }
+}
