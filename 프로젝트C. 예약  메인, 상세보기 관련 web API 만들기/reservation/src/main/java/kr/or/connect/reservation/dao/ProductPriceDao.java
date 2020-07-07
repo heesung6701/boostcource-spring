23b303073d6130120e44482218a575429a9f0043
윤희성 <heesung6701@naver.com>
2020-07-07 20:30:47 +0900

    ADD ProductPriceDao

---
+++ 프로젝트C. 예약  메인, 상세보기 관련 web API 만들기/reservation/src/main/java/kr/or/connect/reservation/dao/ProductPriceDao.java
@@ -0,0 +1,29 @@
+package kr.or.connect.reservation.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import kr.or.connect.reservation.dto.ProductPriceDto;
+import static kr.or.connect.reservation.dao.ProductPriceDaoSqls.SELECT_BY_DISPLAY_INFO_ID;
+
+@Repository
+public class ProductPriceDao {
+  
+  private NamedParameterJdbcTemplate jdbc;
+  private RowMapper<ProductPriceDto> rowMapper = BeanPropertyRowMapper.newInstance(ProductPriceDto.class);
+
+  public ProductPriceDao(DataSource dataSource){
+      this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+  }
+
+  public List<ProductPriceDto> selectByDisplayInfoId(int displayInfoId) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("displayInfoId", displayInfoId);
+    return jdbc.query(SELECT_BY_DISPLAY_INFO_ID, map, rowMapper);
+  }
+}
---
+++ 프로젝트C. 예약  메인, 상세보기 관련 web API 만들기/reservation/src/test/java/kr/or/connect/reservation/dao/ProductPriceDaoTest.java
@@ -0,0 +1,42 @@
+package kr.or.connect.reservation.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import kr.or.connect.reservation.config.ApplicationConfig;
+import kr.or.connect.reservation.dto.ProductPriceDto;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
+public class ProductPriceDaoTest {
+
+  @Autowired
+  DataSource dataSource;
+  
+  @Autowired
+  ProductPriceDao productPriceDao;
+  
+  @Test
+  public void configTest() throws Exception{
+    
+  }
+
+  @Test
+  public void connectionTest() throws Exception {
+    Connection connection = dataSource.getConnection();
+    Assert.assertNotNull(connection);
+  }
+  
+  @Test
+  public void getByIdTest() throws Exception {
+    List<ProductPriceDto> result = productPriceDao.selectByDisplayInfoId(1);
+    Assert.assertNotNull(result);
+    Assert.assertEquals(3, result.size());
+  }
+}
