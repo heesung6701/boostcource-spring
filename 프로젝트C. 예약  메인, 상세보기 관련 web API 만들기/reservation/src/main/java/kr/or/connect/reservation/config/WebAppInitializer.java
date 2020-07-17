7950079959073ee5fb1f5f692118bf3a329f4991
윤희성 <heesung6701@naver.com>
2020-07-17 22:52:01 +0900

    switch projectC and projectD

--- 프로젝트C. 예약  메인, 상세보기 관련 web API 만들기/reservation/src/main/java/kr/or/connect/reservation/config/WebAppInitializer.java
+++ 프로젝트C. 예약  메인, 상세보기 관련 web API 만들기/reservation/src/main/java/kr/or/connect/reservation/config/WebAppInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
   @Override
   protected Class<?>[] getRootConfigClasses() {
-    return new Class<?>[] {ApplicationConfig.class, SecurityConfig.class};
+    return new Class<?>[] {ApplicationConfig.class};
   }
 
   @Override
